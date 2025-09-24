a8a6e87a7e9689223372b09afb2dce9156b31661
AAAtroush <Momen.atroush605@gmail.com>
2025-09-24 14:28:54 +0300

    Main Game Update 0.1v
    So now the following Has been implemented
    
    Grid placement system
    UI to select room sizes
    Preview before placement system
    
    animated the UI
    
    the next commit might be refactoring to state patterns
    
    This update now lets you choose a room size and place it on the grid only if it's valid to do so

---
+++ Layout-Game/Assets/DraggableItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    public int width = 1;
+    public int height = 1;
+
+    [HideInInspector] public Transform parentAfterDrag;
+    [HideInInspector] public List<SlotScript> occupiedSlots = new List<SlotScript>();
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
---
+++ Layout-Game/Assets/GridManagerALT.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class GridManagerALT : MonoBehaviour
+{
+    public int rows;
+    public int cols;
+    public SlotScript[,] slots;
+
+    [Header("References")]
+    public RectTransform gridParent;   // drag your GridPatternImage here
+    public RectTransform itemsContainer;
+
+    void Awake()
+    {
+        slots = new SlotScript[rows, cols];
+
+        // Get all SlotScript components from the grid
+        SlotScript[] allSlots = gridParent.GetComponentsInChildren<SlotScript>();
+
+        for (int i = 0; i < allSlots.Length; i++)
+        {
+            int row = i / cols;
+            int col = i % cols;
+
+            allSlots[i].row = row;
+            allSlots[i].col = col;
+            allSlots[i].gridManager = this;
+
+            slots[row, col] = allSlots[i];
+        }
+    }
+
+    public bool CanPlaceItem(DraggableItem item, int startRow, int startCol)
+    {
+        if (startRow + item.height > rows || startCol + item.width > cols)
+            return false;
+
+        for (int r = 0; r < item.height; r++)
+        {
+            for (int c = 0; c < item.width; c++)
+            {
+                if (slots[startRow + r, startCol + c].occupiedBy != null)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void PlaceItem(DraggableItem item, int startRow, int startCol)
+    {
+        // Clear old slots
+        foreach (var s in item.occupiedSlots)
+            s.occupiedBy = null;
+        item.occupiedSlots.Clear();
+
+        // Fill new slots
+        for (int r = 0; r < item.height; r++)
+        {
+            for (int c = 0; c < item.width; c++)
+            {
+                SlotScript slot = slots[startRow + r, startCol + c];
+                slot.occupiedBy = item;
+                item.occupiedSlots.Add(slot);
+            }
+        }
+
+        // Parent item under ItemsContainer
+        item.transform.SetParent(itemsContainer);
+
+        // Get slot + item rects
+        RectTransform slotRect = slots[startRow, startCol].GetComponent<RectTransform>();
+        RectTransform itemRect = item.GetComponent<RectTransform>();
+
+        float cellWidth = slotRect.rect.width;
+        float cellHeight = slotRect.rect.height;
+
+        // Resize item to cover correct area
+        itemRect.sizeDelta = new Vector2(cellWidth * item.width, cellHeight * item.height);
+
+        // Position to match slot center
+        itemRect.position = slotRect.position;
+    }
+}
